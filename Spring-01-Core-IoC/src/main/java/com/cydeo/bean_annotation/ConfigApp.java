f55b3e91c0e9d36872ffa1812b3b061a63177891
supawadeek <latifetor@gmail.com>
2023-09-14 09:25:31 -0400

    Bean_Annotation

---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/ConfigApp.java
@@ -0,0 +1,15 @@
+package com.cydeo.bean_annotation;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ConfigApp {
+
+    @Bean
+    FullTimeMentor fullTimeMentor(){
+
+        return new FullTimeMentor();
+    }
+}
