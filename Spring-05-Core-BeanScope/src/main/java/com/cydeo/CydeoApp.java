026f98cf54b16885ad5251f86941f1d6c562b1f0
supawadeek <latifetor@gmail.com>
2023-09-17 17:32:00 -0400

    Bean_Scope

--- Spring-05-Core-BeanScope/src/main/java/com/cydeo/CydeoApp.java
+++ Spring-05-Core-BeanScope/src/main/java/com/cydeo/CydeoApp.java
@@ -15,30 +15,31 @@ public class CydeoApp {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        // Bean Scope = default singleton
-        CommentService cs1 = context.getBean(CommentService.class);
-        CommentService cs2 = context.getBean(CommentService.class);
+//        // Bean Scope = default singleton
+//        CommentService cs1 = context.getBean(CommentService.class);
+//        CommentService cs2 = context.getBean(CommentService.class);
+//
+//        // print all the objects: memory_allocation which is the same for both cs1 and cs2
+//        System.out.println(cs1); // com.cydeo.service.CommentService@5a45133e
+//        System.out.println(cs2); // com.cydeo.service.CommentService@5a45133e
+//
+//        // comparing the object
+//        // true: these two objects cs1 and cs2 are the same object
+//        // even though creating in different names
+//        System.out.println(cs1 == cs2); // true
+//
+//        // Bean Scope = Prototype scope
+//        CommentService cs3 = context.getBean(CommentService.class);
+//        CommentService cs4 = context.getBean(CommentService.class);
+//
+//        // print all the objects: memory_allocation which is the same for both cs3 and cs4
+//        System.out.println(cs3); // com.cydeo.service.CommentService@65c7a252
+//        System.out.println(cs4); // com.cydeo.service.CommentService@4d154ccd
+//
+//        // comparing the object
+//        // false: these two objects cs3 and cs4 are different objects
+//        System.out.println(cs3 == cs4); // false
 
-        // print all the objects: memory_allocation which is the same for both cs1 and cs2
-        System.out.println(cs1); // com.cydeo.service.CommentService@5a45133e
-        System.out.println(cs2); // com.cydeo.service.CommentService@5a45133e
-
-        // comparing the object
-        // true: these two objects cs1 and cs2 are the same object
-        // even though creating in different names
-        System.out.println(cs1 == cs2); // true
-
-        // Bean Scope = Prototype scope
-        CommentService cs3 = context.getBean(CommentService.class);
-        CommentService cs4 = context.getBean(CommentService.class);
-
-        // print all the objects: memory_allocation which is the same for both cs3 and cs4
-        System.out.println(cs3); // com.cydeo.service.CommentService@65c7a252
-        System.out.println(cs4); // com.cydeo.service.CommentService@4d154ccd
-
-        // comparing the object
-        // false: these two objects cs3 and cs4 are different objects
-        System.out.println(cs3 == cs4); // false
 
     }
 
--- Spring-05-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
+++ Spring-05-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
@@ -5,12 +5,14 @@ import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 // @Scope("prototype") or can be used
-@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+// @Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@Lazy
 public class CommentService {
 
     private final CommentRepository commentRepository;
@@ -21,6 +23,8 @@ public class CommentService {
                           @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
+
+        System.out.println("Hello");
     }
 
     // Building the Business Logic purpose:  the publishComment ( )
