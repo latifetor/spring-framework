d6e131a588dc70c4f9ec00a12a1995db452addfc
supawadeek <latifetor@gmail.com>
2023-10-07 23:02:07 -0400

    task-1

--- spring-review-IoC/src/main/java/stereotype_annotations/CydeoApp.java
+++ spring-review-IoC/src/main/java/stereotype_annotations/CydeoApp.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import stereotype_annotations.Config.AppConfig;
 import stereotype_annotations.Model.DataStructure;
+import stereotype_annotations.Model.ExtraHours;
 import stereotype_annotations.Model.Microservice;
 
 public class CydeoApp {
@@ -13,10 +14,13 @@ public class CydeoApp {
         ApplicationContext container = new AnnotationConfigApplicationContext(AppConfig.class);
 
         DataStructure dsa = container.getBean(DataStructure.class);
-        dsa.getTotalHours();  // Total hours: 40
+        dsa.getTotalHours();  // Total hours: 40  // Total hours: 50
 
         Microservice mcs = container.getBean(Microservice.class);
         mcs.getTotalHours();  // Total hours: 25
+
+        ExtraHours eh = container.getBean(ExtraHours.class);
+        System.out.println(eh.getHours()); // 10
 
     }
 }
--- spring-review-IoC/src/main/java/stereotype_annotations/Model/DataStructure.java
+++ spring-review-IoC/src/main/java/stereotype_annotations/Model/DataStructure.java
@@ -1,12 +1,33 @@
 package stereotype_annotations.Model;
 
+import lombok.Data;
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@Data
 public class DataStructure {
 
-    public void getTotalHours(){
+    @NonNull
+    ExtraHours extraHours;
 
-        System.out.println("Total hours: " + 40);
+    //@Autowired = Spring version 4.3 only constructor for Dependency injection without annotation
+//    public DataStructure(ExtraHours extraHours) {
+//        this.extraHours = extraHours;
+//    }
+
+    public void getTotalHours(){
+        System.out.println("Total hours: " + (40 + extraHours.getHours()));
     }
+
+    // in order to use this ExtraHours_class inside another class
+    // DataStructure_class need to "HAS" ExtraHours_class to call the method
+    // for the HAS relationship, we need to do dependency injection: DI
+    // Spring will create the DataStructure_object (Bean)
+    // if the class itself has a dependency which is DataStructure HAS dependency with ExtraHours_class
+    // or the class is a dependency of the other class, we need to use @Component
+    // because it is part of the chain,
+    // to be able to successfully create the DataStructure_class, Object or Beans, we also need the ExtraHours_Bean
+    // because without ExtraHours_class it will not able be created
 }
