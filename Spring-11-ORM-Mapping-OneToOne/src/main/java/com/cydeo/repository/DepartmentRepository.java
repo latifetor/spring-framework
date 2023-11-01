19c0fb1e8f4a4d51ab0f235b71f2fe994fa7585a
supawadeek <latifetor@gmail.com>
2023-11-01 13:44:32 -0400

    ORM Part-3

--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/repository/DepartmentRepository.java
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/repository/DepartmentRepository.java
@@ -1,8 +1,8 @@
-package com.cydeo.repository;
-
-import com.cydeo.entity.Department;
+package com.cydeo.repository;import com.cydeo.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartmentRepository extends JpaRepository<Department,Integer> {
 
 }
+
+
