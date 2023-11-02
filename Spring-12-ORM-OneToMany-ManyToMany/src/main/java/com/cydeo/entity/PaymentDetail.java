27a348619a59eb36041a14b739a703e2e9bdc5d7
supawadeek <latifetor@gmail.com>
2023-11-02 18:13:47 -0400

    ORM Part-4

---
+++ Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/cydeo/entity/PaymentDetail.java
@@ -0,0 +1,35 @@
+package com.cydeo.entity;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "paymentDetails")
+@Data
+@NoArgsConstructor
+public class PaymentDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal commissionAmount;
+    private BigDecimal merchantPayoutAmount;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate payoutDate;
+
+    @OneToOne(mappedBy = "paymentDetail",cascade=CascadeType.ALL)
+    @JoinColumn(name = "payment_id")
+    private Payment payment;
+
+    public PaymentDetail(BigDecimal commissionAmount, BigDecimal merchantPayoutAmount, LocalDate payoutDate) {
+        this.commissionAmount = commissionAmount;
+        this.merchantPayoutAmount = merchantPayoutAmount;
+        this.payoutDate = payoutDate;
+    }
+}
