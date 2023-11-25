266a52ee43f49dd48ab17cd9c9c14c3c56110e1a
supawadeek <latifetor@gmail.com>
2023-11-25 12:48:47 -0500

    Spring ORM Queries: Derived Queries

--- Spring-14-ORM-Queries/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,5 +1,7 @@
 package com.cydeo.bootstrap;
 
+import com.cydeo.repository.DepartmentRepository;
+import com.cydeo.repository.EmployeeRepository;
 import com.cydeo.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,9 +10,13 @@ import org.springframework.stereotype.Component;
 public class DataGenerator implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
+    private final DepartmentRepository departmentRepository;
+    private final EmployeeRepository employeeRepository;
 
-    public DataGenerator(RegionRepository regionRepository) {
+    public DataGenerator(RegionRepository regionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository) {
         this.regionRepository = regionRepository;
+        this.departmentRepository = departmentRepository;
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
@@ -24,6 +30,27 @@ public class DataGenerator implements CommandLineRunner {
         System.out.println("findByCountryIsContainingOrderByCountry = " + regionRepository.findByCountryIsContainingOrderByCountry("Asia"));
         System.out.println("findTopTwoByCountry = " + regionRepository.findTopTwoByCountry("Canada"));
         System.out.println("findTop2ByCountry = " + regionRepository.findTop2ByCountry("Canada"));
+
+        System.out.println("-------------DEPARTMENT------------");
+
+        System.out.println("findByDepartment = " + departmentRepository.findByDepartment("Toys"));
+        System.out.println("findByDivisionIs = " + departmentRepository.findByDivisionIs("Outdoors"));
+        System.out.println("findByDivisionEquals = " + departmentRepository.findByDivisionEquals("Outdoors"));
+        System.out.println("findDistinctTop3ByDivisionContains = " + departmentRepository.findDistinctTop3ByDivisionContains("Hea"));
+
+        System.out.println("-------------EMPLOYEE------------");
+
+        System.out.println("findByEmail = " + employeeRepository.findByEmail("btrow5@technorati.com"));
+        System.out.println("findByFirstNameAndLastNameOrEmail = " + employeeRepository.findByFirstNameAndLastNameOrEmail("Annora","Bendelow","abendelowg@google.com.hk"));
+        //System.out.println("findDistinctTop3BySalaryIsLessThan = " + employeeRepository.findDistinctTop3BySalaryIsLessThan(100000));
+        //System.out.println("findByEmailIsNull = " + employeeRepository.findByEmailIsNull());
+        //System.out.println("findBySalaryIsGreaterThanEqualOrderBySalaryDesc = " + employeeRepository.findBySalaryIsGreaterThanEqualOrderBySalaryDesc(120000));
+        //System.out.println("findByLastNameStartingWith = "+employeeRepository.findByLastNameStartingWith("E"));
+
+        System.out.println("-------------EMPLOYEE------------");
+
+
+
 
     }
 }
