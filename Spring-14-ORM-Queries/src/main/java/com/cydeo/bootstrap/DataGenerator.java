1d38cb7db9ceb9461e7b6b13956736d58aa688d2
supawadeek <latifetor@gmail.com>
2023-11-25 01:47:07 -0500

    Spring ORM Queries: Derived Queries

---
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -0,0 +1,26 @@
+package com.cydeo.bootstrap;
+
+import com.cydeo.repository.RegionRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataGenerator implements CommandLineRunner {
+
+    private final RegionRepository regionRepository;
+
+    public DataGenerator(RegionRepository regionRepository) {
+        this.regionRepository = regionRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("-------------REGION------------");
+
+        System.out.println("findByCountry:" + regionRepository.findByCountry("Canada"));
+        System.out.println("findDistinctByCountry: " + regionRepository.findDistinctByCountry("Canada"));
+
+
+    }
+}
